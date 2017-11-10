5bee947b099ae0e8b80c3bb85d13522b54fd033b
thomas focqueu <thomas.focqueu@hotmail.fr>
2017-11-10 11:43:38 +0100

    ajout des routes en annotation

---
+++ src/ProjectBundle/Controller/ProjectController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ProjectBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+
+class ProjectController extends Controller
+{
+    /**
+     * @Route("/accueil")
+     */
+
+    public function indexAction()
+    {
+        return $this->render('ProjectBundle:ProjectFront:accueil.html.twig');
+    }
+}
---
+++ src/ProjectBundle/Resources/views/ProjectFront/accueil.html.twig
@@ -0,0 +1,1 @@
+bienvenue sur l'accueil
