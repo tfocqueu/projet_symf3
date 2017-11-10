cd2afaa8b1b3bbb661aa64ece0c5fc44370f646d
thomas focqueu <thomas.focqueu@hotmail.fr>
2017-11-10 15:01:17 +0100

    add fosUser

--- src/ProjectBundle/Entity/Utilisateur.php
+++ src/ProjectBundle/Entity/Utilisateur.php
@@ -2,6 +2,8 @@
 
 namespace ProjectBundle\Entity;
 
+use Doctrine\ORM\Query\Expr\Base;
+use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -10,7 +12,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="utilisateur")
  * @ORM\Entity(repositoryClass="ProjectBundle\Repository\UtilisateurRepository")
  */
-class Utilisateur
+class Utilisateur extends BaseUser
 {
     /**
      * @var int
@@ -19,7 +21,41 @@ class Utilisateur
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    private $id;
+    protected $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="prenom", type="string", length=255)
+     */
+    private $prenom;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="adresse", type="string", length=255)
+     */
+    private $adresse;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="anneeobtention", type="integer")
+     */
+    private $anneeobtention;
+
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
 
 
     /**
