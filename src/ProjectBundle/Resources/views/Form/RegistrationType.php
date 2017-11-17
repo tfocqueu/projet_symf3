b7ea00bf657a5cd9db9303ef06905d1e23b69bf3
thomas focqueu <thomas.focqueu@hotmail.fr>
2017-11-17 08:53:56 +0100

    Page de login en cours

---
+++ src/ProjectBundle/Resources/views/Form/RegistrationType.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace ProjectBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+    }
+
+    public function getParent()
+    {
+        return 'fos_user_registration';
+    }
+
+    public function getName()
+    {
+        return 'app_user_registration';
+    }
+}
