73d8daa6fa8764e01db2e6fa7862d9787ecaf032
Maxime Bordas <maxime.bordas&@gmail.com>
2017-11-28 15:33:05 +0100

    add Entreprise and observation field for stage add

--- src/ProjectBundle/Form/StageType.php
+++ src/ProjectBundle/Form/StageType.php
@@ -6,6 +6,7 @@ use ProjectBundle\Entity\Technologie;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -39,6 +40,16 @@ class StageType extends AbstractType
                         'class'=> 'form-control',
                         'required'=>true,
                     ]
+                ))
+
+                ->add('observation',TextareaType::class,array(
+                    'attr' => [
+                        'class' => 'form-control',
+                        'required' => true,
+                    ]
+                ))
+                ->add('enteprises',EntityType::class,array(
+                    'class' => 'ProjectBundle\Entity\Entreprise'
                 ))
                 ->add('visits',EntityType::class,array(
                     'class' => 'ProjectBundle\Entity\Visite'
