8fe1901499239b4b2c34fe32179884d98e6d5204
thomas focqueu <thomas.focqueu@hotmail.fr>
2017-12-04 21:19:23 +0100

    sensiolabs test

--- src/ProjectBundle/Controller/SecurityController.php
+++ src/ProjectBundle/Controller/SecurityController.php
@@ -79,20 +79,11 @@ class SecurityController extends BaseController
         }
 
         // last username entered by the user
-
         $lastUsername = (null === $session) ? '' : $session->get($lastUsernameKey);
 
         $csrfToken = $this->has('security.csrf.token_manager') ? $this->get('security.csrf.token_manager')->getToken('authenticate')->getValue() : null;
 
-        return $this->renderLogin(array(
-
-            'last_username' => $lastUsername,
-
-            'error' => $error,
-
-            'csrf_token' => $csrfToken,
-
-        ));
+        return $this->renderLogin(array('last_username' => $lastUsername, 'error' => $error,'csrf_token' => $csrfToken,));
 
     }
 
